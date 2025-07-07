6b149e9403c80f3d1cf45c62f6c1b2ad120bab7f
ahmad9059 <ahmadhassan9059@gmail.com>
2025-07-07 13:54:19 +0500

    gtk theme addded

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -58,7 +58,7 @@ mkdir -p "$BACKUP_DIR"
 
 # Copy new dotfiles
 echo -e "${GREEN}📄 Copying new dotfiles...${NC}"
-cp -r "$REPO_DIR/.config/" ~/.config/
+cp -r "$REPO_DIR/.config/"* ~/.config/
 cp "$REPO_DIR/.zshrc" ~/
 cp "$REPO_DIR/.tmux.conf" ~/
 
@@ -113,6 +113,49 @@ else
 fi
 
 echo -e "${GREEN}✅ SDDM theme set to '$SDDM_THEME_NAME'.${NC}"
+
+
+# Gtk theme setup
+echo -e "${GREEN}🎨 Updating GTK theme settings...${NC}"
+
+# Create config directories if not exist
+mkdir -p ~/.config/gtk-3.0
+mkdir -p ~/.config/gtk-4.0
+
+# GTK 3.0 settings
+cat <<EOF > ~/.config/gtk-3.0/settings.ini
+[Settings]
+gtk-theme-name=Andromeda-dark
+gtk-icon-theme-name=dracula-icons-main
+gtk-font-name=Adwaita Sans 11
+gtk-cursor-theme-name=Future-black-cursors
+gtk-cursor-theme-size=24
+gtk-toolbar-style=GTK_TOOLBAR_ICONS
+gtk-toolbar-icon-size=GTK_ICON_SIZE_LARGE_TOOLBAR
+gtk-button-images=1
+gtk-menu-images=1
+gtk-enable-event-sounds=1
+gtk-enable-input-feedback-sounds=0
+gtk-xft-antialias=1
+gtk-xft-hinting=1
+gtk-xft-hintstyle=hintslight
+gtk-xft-rgba=rgb
+gtk-application-prefer-dark-theme=1
+EOF
+
+# GTK 4.0 settings
+cat <<EOF > ~/.config/gtk-4.0/settings.ini
+[Settings]
+gtk-theme-name=Andromeda-dark
+gtk-icon-theme-name=dracula-icons-main
+gtk-font-name=Adwaita Sans 11
+gtk-cursor-theme-name=Future-black-cursors
+gtk-cursor-theme-size=24
+gtk-application-prefer-dark-theme=1
+EOF
+
+echo -e "${GREEN}✅ GTK settings applied successfully!${NC}"
+
 
 # Grub BootLoader Theme (Vimix)
 echo -e "${GREEN}🧩 Checking for GRUB...${NC}"
