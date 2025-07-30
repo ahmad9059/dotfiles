fe19c04454698565e82631d6c1df70a8dbf57772
ahmad9059 <ahmadhassan9059@gmail.com>
2025-07-30 21:09:08 +0500

    nvim added

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,7 +1,7 @@
 require "nvchad.options"
 
 -- add yours here!
-vim.opt.encoding = "utf-8"
-vim.opt.fileencoding = "utf-8"
+
+
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
