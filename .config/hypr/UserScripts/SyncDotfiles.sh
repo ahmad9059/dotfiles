17fd6524e2a0b3ee4bc05651f17a6317a5694cef
ahmad9059 <ahmadhassan9059@gmail.com>
2025-08-17 12:39:54 +0500

    Sync local changes 2025-08-17 12:39:54

--- .config/hypr/UserScripts/SyncDotfiles.sh
+++ .config/hypr/UserScripts/SyncDotfiles.sh
@@ -48,23 +48,6 @@ rsync -av --delete "$HOME/.tmuxifier/layouts/" "$REPO_DIR/.tmuxifier/layouts/"
 [ -f "$HOME/.zshrc" ] && cp "$HOME/.zshrc" "$REPO_DIR/.zshrc"
 [ -f "$HOME/.tmux.conf" ] && cp "$HOME/.tmux.conf" "$REPO_DIR/.tmux.conf"
 
-### --- PRE-SYNC MODIFICATIONS --- ###
-
-# 1. Modify kb_options line
-if [ -f "$USER_SETTINGS" ]; then
-  sed -i 's/^kb_options = ctrl:nocaps$/kb_options = /' "$USER_SETTINGS"
-fi
-
-# 2. Replace label block
-if [ -f "$USER_SETTINGS" ]; then
-  sed -i '/label {/,/}/c\
-label {\n     monitor =\n    text = cmd[update:1000] echo -e "$(LC_TIME=en_US.UTF-8 date +"%A, %B %d")"\n    color = rgba(216, 222, 233, 0.90)\n    font_size = 25\n    font_family = SF Pro Display Semibold\n    position = 0, 350\n    halign = center\n    valign = center\n}' "$USER_SETTINGS"
-fi
-
-# 3. Break symlinks for waybar
-rm -rf "$WAYBAR_CONFIG"
-rm -rf "$WAYBAR_STYLE"
-
 echo -e "${GREEN}Local changes synced to repo.${NC}"
 
 # Commit and push changes
