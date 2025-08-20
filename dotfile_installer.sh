1e8691835cfa1ec37a0f71f8d150f6a0bccb40a6
ahmad9059 <ahmadhassan9059@gmail.com>
2025-08-20 07:01:25 +0500

    Sync local changes 2025-08-20 07:01:25

--- dotfile_installer.sh
+++ dotfile_installer.sh
@@ -44,6 +44,12 @@ DESKTOP_DIR="$HOME/.local/share/applications"
 ICON_DIR="$HOME/.local/share/icons/apps"
 BROWSER="chromium"
 
+# ===========================
+# Log Details
+# ===========================
+mkdir -p "$HOME/installer_log"
+LOG_FILE="$HOME/installer_log/boot_file.log"
+
 # =============================
 # Packages list
 # =============================
