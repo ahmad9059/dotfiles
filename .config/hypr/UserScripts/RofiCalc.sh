79da8e2a21ba97bbb4cdbbd20cbca91b907a3f71
ahmad9059 <ahmadhassan9059@gmail.com>
2025-08-21 11:12:01 +0500

    Sync local changes 2025-08-21 11:12:01

--- .config/hypr/UserScripts/RofiCalc.sh
+++ .config/hypr/UserScripts/RofiCalc.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# /* ---- 💫 https://github.com/JaKooLit 💫 ---- */
 # /* Calculator (using qalculate) and rofi */
 # /* Submitted by: https://github.com/JosephArmas */
 
@@ -7,24 +6,24 @@ rofi_theme="$HOME/.config/rofi/config-calc.rasi"
 
 # Kill Rofi if already running before execution
 if pgrep -x "rofi" >/dev/null; then
-    pkill rofi
+  pkill rofi
 fi
 
 # main function
 
 while true; do
-    result=$(
-        rofi -i -dmenu \
-            -config $rofi_theme \
-            -mesg "$result      =    $calc_result"
-    )
+  result=$(
+    rofi -i -dmenu \
+      -config $rofi_theme \
+      -mesg "$result      =    $calc_result"
+  )
 
-    if [ $? -ne 0 ]; then
-        exit
-    fi
+  if [ $? -ne 0 ]; then
+    exit
+  fi
 
-    if [ -n "$result" ]; then
-        calc_result=$(qalc -t "$result")
-        echo "$calc_result" | wl-copy
-    fi
+  if [ -n "$result" ]; then
+    calc_result=$(qalc -t "$result")
+    echo "$calc_result" | wl-copy
+  fi
 done
--- .config/hypr/UserScripts/WallpaperAutoChange.sh
+++ .config/hypr/UserScripts/WallpaperAutoChange.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
 # source https://wiki.archlinux.org/title/Hyprland#Using_a_script_to_change_wallpaper_every_X_minutes
 
 # This script will randomly go through the files of a directory, setting it
@@ -11,10 +10,10 @@ wallust_refresh=$HOME/.config/hypr/scripts/RefreshNoWaybar.sh
 
 focused_monitor=$(hyprctl monitors | awk '/^Monitor/{name=$2} /focused: yes/{print name}')
 
-if [[ $# -lt 1 ]] || [[ ! -d $1   ]]; then
-	echo "Usage:
+if [[ $# -lt 1 ]] || [[ ! -d $1 ]]; then
+  echo "Usage:
 	$0 <dir containing images>"
-	exit 1
+  exit 1
 fi
 
 # Edit below to control the images transition
@@ -25,15 +24,15 @@ export SWWW_TRANSITION_TYPE=simple
 INTERVAL=1800
 
 while true; do
-	find "$1" \
-		| while read -r img; do
-			echo "$((RANDOM % 1000)):$img"
-		done \
-		| sort -n | cut -d':' -f2- \
-		| while read -r img; do
-			swww img -o $focused_monitor "$img" 
-			$wallust_refresh
-			sleep $INTERVAL
-			
-		done
+  find "$1" |
+    while read -r img; do
+      echo "$((RANDOM % 1000)):$img"
+    done |
+    sort -n | cut -d':' -f2- |
+    while read -r img; do
+      swww img -o $focused_monitor "$img"
+      $wallust_refresh
+      sleep $INTERVAL
+
+    done
 done
