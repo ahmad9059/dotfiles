64b873c1e66d12a3a37b73d08d1d16e972a6514f
ahmad9059 <ahmadhassan9059@gmail.com>
2025-08-26 23:13:57 +0500

    c

--- install.sh
+++ install.sh
@@ -89,31 +89,6 @@ else
   fi
 fi
 
-# ======================
-# Initial Setup
-# ======================
-# echo "${NOTE} Adding Chaotic-AUR repository...${RESET}"
-# # Import Chaotic AUR keys
-# sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-# sudo pacman-key --lsign-key 3056513887B78AEB
-# # Install chaotic-keyring and chaotic-mirrorlist
-# sudo pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
-# sudo pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-# # Append chaotic-aur repo if not already present
-# if ! grep -q "\[chaotic-aur\]" /etc/pacman.conf; then
-#   echo -e "\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf
-#   echo "${OK} Chaotic-AUR repo added to pacman.conf${RESET}"
-# else
-#   echo "${NOTE} Chaotic-AUR repo already exists in pacman.conf${RESET}"
-# fi
-# # Sync and update system
-# echo "${NOTE} Updating system with Chaotic-AUR mirrors...${RESET}"
-# sudo pacman -Syu --noconfirm
-# # Install wallust and quickshell
-# echo "${NOTE} Installing wallust and quickshell...${RESET}"
-# sudo pacman -S --noconfirm wallust quickshell
-# echo "${OK} Installation of Initial Setup  Complete!${RESET}"
-
 # ===========================
 # Run Arch-Hyprland installer
 # ===========================
