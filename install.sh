504578c1df041208388f65376567afd8eb554ad9
ahmad9059 <ahmadhassan9059@gmail.com>
2025-08-26 21:28:12 +0500

    c

--- install.sh
+++ install.sh
@@ -92,7 +92,6 @@ fi
 # ======================
 # Initial Setup
 # ======================
-
 echo "${NOTE} Adding Chaotic-AUR repository...${RESET}"
 # Import Chaotic AUR keys
 sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
